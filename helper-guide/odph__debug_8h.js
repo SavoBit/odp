2b77f3a7569075601e3642ba4674d71be540fa6d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-04 16:08:13 +0000

    Deploy OpenDataPlane/odp to github.com/OpenDataPlane/odp.git:gh-pages

---
+++ helper-guide/odph__debug_8h.js
@@ -0,0 +1,15 @@
+var odph__debug_8h =
+[
+    [ "ODPH_ASSERT", "odph__debug_8h.html#a428417b3592d2a4b0fe02c26f3d44fa6", null ],
+    [ "ODPH_LOG", "odph__debug_8h.html#a33c58bcaf10f7e9dd0b3bbfc708199d1", null ],
+    [ "ODPH_DBG", "odph__debug_8h.html#a3011267e9d7aaad0bbb84e23b638793d", null ],
+    [ "ODPH_ERR", "odph__debug_8h.html#a3fddc8648e6835b9ac5e17b6da2ca53c", null ],
+    [ "ODPH_ABORT", "odph__debug_8h.html#a68f0b6b1b22312db270598be773dcebd", null ],
+    [ "ODPH_UNUSED", "odph__debug_8h.html#a21ce2bf769fa0bebe14edf1ab336461c", null ],
+    [ "odph_log_level_e", "odph__debug_8h.html#ada0ba599ecfc511f714c0a0510db89fd", null ],
+    [ "odph_log_level", "odph__debug_8h.html#af7d39fee23645f660e0684f1d98414a6", [
+      [ "ODPH_LOG_DBG", "odph__debug_8h.html#af7d39fee23645f660e0684f1d98414a6af75888698bcb9713285f3ad32ea4d2b6", null ],
+      [ "ODPH_LOG_ERR", "odph__debug_8h.html#af7d39fee23645f660e0684f1d98414a6a2fd7ee11c1412f6201c265b71822a081", null ],
+      [ "ODPH_LOG_ABORT", "odph__debug_8h.html#af7d39fee23645f660e0684f1d98414a6a687280104ffe35185685011e1c7256ee", null ]
+    ] ]
+];
---
+++ helper-guide/search/enums_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['odph_5fchksum_5fop_5ft',['odph_chksum_op_t',['../group__odph__chksum.html#ga5f3b415ea4daef7819331058f582093e',1,'chksum.h']]],
+  ['odph_5flog_5flevel',['odph_log_level',['../odph__debug_8h.html#af7d39fee23645f660e0684f1d98414a6',1,'odph_debug.h']]]
+];
